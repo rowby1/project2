0eeedbb057dda531ed7a54b6564f7c5362c69f5d
rowby1 <59141956+rowby1@users.noreply.github.com>
2022-01-07 17:36:41 +0900

    0107 1736

---
+++ Prj2/src/mboard/controller/servlet/GetDBDate.java
@@ -0,0 +1,32 @@
+package mboard.controller.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import mboard.dao.CountryCovidStatusDao;
+
+@WebServlet("/getdbdate")
+public class GetDBDate extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		
+		CountryCovidStatusDao ccsDao = new CountryCovidStatusDao();
+		int date = ccsDao.getDBDate();
+
+		PrintWriter out = response.getWriter();
+		out.print(date);
+		out.close();
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
